0518902ffd13ac23aaaf000f9cfb3d508021d3a1
Pirate-Emperor <piratekingrahul@gmail.com>
2024-07-26 22:42:00 +0100

    Add uvm\AES_package.sv

---
+++ uvm/AES_package.sv
@@ -0,0 +1,16 @@
+package aes_pkg;
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "AES_sequence_item.svh"
+`include "AES_sequence.svh"
+`include "AES_sequencer.svh"
+`include "AES_driver.svh"
+`include "AES_monitor.svh"
+`include "AES_agent.svh"
+`include "AES_scoreboard.svh"
+`include "AES_subscriber.svh"
+`include "AES_env.svh"
+`include "AES_test.svh"
+
+endpackage
